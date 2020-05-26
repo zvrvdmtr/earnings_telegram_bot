b952e9e7c0c51ed8039988dec67871fa2cb3070f
zveridze <zverevdmitri.87@gmail.com>
2020-05-26 20:54:49 +0300

    Moved handlers to handlers dir. Add error handler.

--- src/bot.py
+++ src/bot.py
@@ -1,52 +1,21 @@
 from telegram.ext import Updater, CommandHandler, MessageHandler, Filters, ConversationHandler
-from telegram import ReplyKeyboardMarkup, ReplyKeyboardRemove
+from handlers import start_conversation, ticker, processing, skip_ticker, stop, error_handler
 import settings
-from services import finnhub_connection
+import re
 
 PROXY = {'proxy_url': settings.SOCKS_URL,
          'urllib3_proxy_kwargs': {'username': settings.SOCKS_USERNAME,
                                   'password': settings.SOCKS_PASSWORD}}
 
 
-PERIOD = ['Year', 'Month', 'Week', 'Day']
-
-
-def start(update, context):
-    replies = [PERIOD]
-    update.message.reply_text('Choose wisely', reply_markup=ReplyKeyboardMarkup(replies, one_time_keyboard=True))
-    return 'TICKER'
-
-
-def ticker(update, context):
-    context.user_data[update.message.from_user.id] = {'period': settings.PERIODS[update.message.text.upper()]}
-    update.message.reply_text('Enter ticker. Or send /skip.', reply_markup=ReplyKeyboardRemove())
-    return 'PROCESSING'
-
-
-def skip_ticker(update, context):
-    return 'PROCESSING'
-
-
-def processing(update, context):
-    context.user_data[update.message.from_user.id].update({'ticker': update.message.text})
-    update.message.reply_text('Start processing. Wait.')
-    finnhub_connection(period=context.user_data[update.message.from_user.id]['period'])
-    update.message.reply_text('Your report is ready.')
-    return ConversationHandler.END
-
-
-def stop(update, context):
-    update.message.reply_text('Conversation end', reply_markup=ReplyKeyboardRemove())
-    return ConversationHandler.END
-
-
 def main():
     updater = Updater(settings.TELEGRAM_TOKEN, use_context=True, request_kwargs=PROXY)
     dp = updater.dispatcher
+
     conv_handler = ConversationHandler(
-        entry_points=[CommandHandler("start", start)],
+        entry_points=[CommandHandler("start", start_conversation)],
         states={
-            'TICKER': [MessageHandler(Filters.regex('^(Year|Week|Month|Day)$'), ticker)],
+            'TICKER': [MessageHandler(Filters.regex(re.compile('^(Year|Week|Month|Day)$', re.IGNORECASE)), ticker)],
             'PROCESSING': [
                 MessageHandler(Filters.text, processing),
                 CommandHandler('skip', skip_ticker)
@@ -55,6 +24,7 @@ def main():
         fallbacks=[CommandHandler("stop", stop)]
     )
     dp.add_handler(conv_handler)
+    dp.add_error_handler(error_handler)
     updater.start_polling()
     updater.idle()
 
