99cd13c1bf8f92ca78338c599dbc724bab17d302
zveridze <zverevdmitri.87@gmail.com>
2020-05-31 16:56:25 +0300

    Rewrite finnhub service. Add readme. Add debouce to handler.

--- src/services.py
+++ src/services.py
@@ -3,13 +3,27 @@ from settings import FINNHUB_TOKEN
 from datetime import datetime, timedelta
 
 
-def finnhub_connection(period=None, ticker=None):
-    url = 'https://finnhub.io/api/v1/calendar/earnings'
-    params = {
-        'from': datetime.now().date(),
-        'to': datetime.now().date() + timedelta(days=period),
-        'symbol': ticker,
-        'token': FINNHUB_TOKEN
-    }
-    response = requests.get(url=url, params=params)
-    return response.text
+class FunnhubService:
+
+    def __init__(self):
+        self.url = 'https://finnhub.io/api/v1/'
+
+    def get_earnings(self, period=None, ticker=None):
+        url = self.url + 'calendar/earnings'
+        params = {
+            'from': datetime.now().date(),
+            'to': datetime.now().date() + timedelta(days=period),
+            'symbol': ticker,
+            'token': FINNHUB_TOKEN
+        }
+        response = requests.get(url=url, params=params)
+        return response.text
+
+    def get_name_by_ticker(self, ticker):
+        url = self.url + 'stock/profile2'
+        params = {
+            'symbol': ticker,
+            'token': FINNHUB_TOKEN
+        }
+        response = requests.get(url=url, params=params)
+        return response.text
